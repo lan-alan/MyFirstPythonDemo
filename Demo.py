4ed28bd108f86a051d7db95c18648e0d761aabf0
Alan <lanhonghui100@gmail.com>
2018-01-05 11:31:04 +0800

    更新

--- Demo.py
+++ Demo.py
@@ -7,3 +7,15 @@ print(i);
 
 s = "This is a multi-line string.This is the second line.";
 print(s);
+
+print("Python真的如此的神奇？");
+
+i = 5;
+j = 4;
+c = i*j;
+print(c);
+
+y = 5;
+x = 6;
+y <= x;
+print(y>=x);
